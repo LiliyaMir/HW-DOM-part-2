bd5db78b789da80a80ffe3d1620a1c06a4e0ce2e
Lilia Mironova <lilyamironova67@gmail.com>
2022-04-22 13:03:27 +0300

    finish

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ let setFigure = document.querySelector('select');
 
 let setedFigure = setFigure.selectedOptions;
 setFigure.addEventListener('click', function () {
-  let setFigLab = setedFigure[0].label;
-  if (setFigLab === 'прямоугольник') {
+  let setFigLab = setedFigure[0].value;
+  if (setFigLab === 'rectangle') {
     figure.setAttribute('class', 'rectangle')
   }
-  else if (setFigLab === 'круг') {
+  else if (setFigLab === 'circle') {
     figure.setAttribute('class', 'circle')
   }
   else {
